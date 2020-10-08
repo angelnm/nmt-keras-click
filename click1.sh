c20bf0abe4418bfad6971d45f0b90fa1bd95be4c
angelnm <angel.nm@hotmail.es>
2020-10-08 10:06:18 +0200

    Better Comments

--- click1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-folder=$1
-source=$2
-target=$3
-models=$4
-number=$5
-python3 click.py -ds "./../xerox/model/"$source"-"$target"/Dataset_xerox_"$source$target".pkl" -src ./../xerox/preraw/tst/$folder/test.$source -trg ./../xerox/preraw/tst/$folder/test.$target -d ./../xerox/results/$source_$target.txt --models ./../xerox/model/$source-$target/$models --prefix -ma $number
--- click_char.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python3 click_char.py -ds ./datasets/Dataset_xerox_enfr.pkl -src ./../xerox/preraw/test.en -trg ./../xerox/preraw/test.fr -d ./../out.txt --models ./trained_models/best_xerox_512_50/update_37500 --prefix -ma $1
